2492e47c335cb9474a9a93be5031a63208f9e9fa
GZ-99 <gabrielvilchezvite@gmail.com>
2026-05-28 12:23:20 -0500

    feat(supplier): add supplier aggregate with identifier, name and address.

---
+++ Acme.OOP/SCM/Domain/Model/Aggregates/Supplier.cs
@@ -0,0 +1,15 @@
+﻿using Acme.OOP.Shared.Domain.Model.ValueObjects;
+
+namespace Acme.OOP.SCM.Domain.Model.Aggregates;
+/// <summary>
+/// Represents a supplier aggregate in the 'SCM' bounded context. 
+/// </summary>
+/// <param name="identifier">The unique identifier for the supplier.</param>
+/// <param name="name">The name of the supplier.</param>
+/// <param name="address">The <see cref="Address"/> of the supplier.</param>
+public class Supplier(string identifier, string name, Address address)
+{
+    public string Identifier { get; } = identifier ?? throw new ArgumentNullException(nameof(identifier));
+    public string Name { get; } = name ?? throw new ArgumentNullException(nameof(name));
+    public Address Address { get; } = address ?? throw new ArgumentNullException(nameof(address));  
+}
